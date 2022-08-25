9f496691b8f2569b01990b053472410af2c17260
Guilherme Eduardo T Carvalho <91907319+guiduzera@users.noreply.github.com>
2022-08-25 17:32:38 -0300

    preparando o ambiente e criando as migrations do sequelize para cirar as tabelas e seus campos

---
+++ src/database/migrations/20220825201134-create-categories.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('PostCategories', {
+      postId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'BlogPosts',
+          key: 'id',
+        },
+        field: 'postId',
+        primaryKey: true
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Categories',
+          key: 'id',
+        },
+        field: 'categoryId',
+        primaryKey: true
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('PostCategories');
+  }
+};
